f514fcc11b0dccad75a52cde03a37bea72691159
Justin Lang <jlang61@ucsb.edu>
2024-12-02 18:57:21 -0800

    adding oneclasssvm

--- scripts/drafts/climate.py
+++ scripts/drafts/climate.py
@@ -14,7 +14,18 @@ df["date"] = df["date"].astype(np.int64) / (10**9)
 
 print(df.head())
 # Start anomaly detection
-model = IsolationForest()
+model = IsolationForest(contamination=0.1, random_state=42)
 model.fit(df)
 anomalies = model.predict(df)
+np.set_printoptions(threshold=np.inf)
 print(anomalies)
+
+print(anomalies[anomalies == -1].shape)
+
+from sklearn.svm import OneClassSVM
+
+svm = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+svm.fit(df)
+anomalies = svm.predict(df)
+print(anomalies)
+print(anomalies[anomalies == -1].shape)
